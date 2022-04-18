0f224231be726a07098a3c2c352d16357b69c2d2
Dereje <95926729+Dereje-getu-tadesse-10@users.noreply.github.com>
2022-04-18 17:58:38 +0200

    [Fin api 🍀]
    fin de l'intégration de l'api meteo le widget fonctionne parfaitement

--- app.js
+++ app.js
@@ -2,7 +2,15 @@ const openWeather = document.querySelector('.container-title');
 const content = document.querySelector(' #weather-container');
 const closeApp = document.querySelector('.tempMin');
 const form = document.querySelector('form');
-const input = document.queryCommandValue('input');
+const input = document.querySelector('#searchCity');
+const city = document.querySelector('.city');
+const currentTime = document.querySelector('.currentTime');
+const logo = document.querySelector('#logoTemp');
+let src = logo.getAttribute('src')
+const description = document.querySelector('.desc');
+const tempMin = document.querySelector('.tempMin');
+const tempMax = document.querySelector('.tempMax');
+
 
 toggleModale('.container-title','#weather-container','block')
 toggleModale('.fa-square-xmark', '#weather-container','')
@@ -13,12 +21,32 @@ function toggleModale(el, content,style){
         content.style.display=style;
     })
 }
-const apiKey = '6bb83d7cf2b4aee765f7bd3f53260c22';
+   
 
 form.addEventListener('click', (e)=>{
     e.preventDefault();
     let inp = input.value;
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${inp},${inp}&appid=${apiKey}`;
-    fetcj
-
+    console.log(inp);
+    callApi(inp);
 })
+callApi('Paris');
+function callApi(val) {
+    const apiKey = '6bb83d7cf2b4aee765f7bd3f53260c22';
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${val}&appid=${apiKey}&units=metric&lang=fr`
+    fetch(url)
+    .then((res)=>{
+        return res.json();
+    })
+    .then((data)=>{
+        console.log(data);
+        city.textContent = data.name;
+        currentTime.textContent = data.main.temp;
+        let src = logo.getAttribute('src')
+        let img = data.weather[0].icon;
+        src = `http://openweathermap.org/img/wn/${img}.png`;
+        logo.setAttribute('src', src);
+        description.textContent = data.weather[0].description;
+        tempMin.textContent = 'Temp Max ' + data.main.temp_min;
+        tempMax.textContent = 'Temp Max ' + data.main.temp_max;
+    })
+}
