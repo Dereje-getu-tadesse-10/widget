0617536165ad8d4a2a61d13c60319d47dd113204
Dereje <95926729+Dereje-getu-tadesse-10@users.noreply.github.com>
2022-04-18 17:03:19 +0200

    [Integration api open weather 🍀]

--- app.js
+++ app.js
@@ -1,19 +1,24 @@
 const openWeather = document.querySelector('.container-title');
 const content = document.querySelector(' #weather-container');
 const closeApp = document.querySelector('.tempMin');
+const form = document.querySelector('form');
+const input = document.queryCommandValue('input');
 
-// openWeather.addEventListener('click', ()=>{
-//     content.style.display="block";
-// })
-// closeApp.addEventListener('click', ()=>{
-//     content.style.display="";
-// })
-Display('.container-title','#weather-container','block')
-Display('.tempMin', '#weather-container','')
-function Display(el, content,style){
+toggleModale('.container-title','#weather-container','block')
+toggleModale('.fa-square-xmark', '#weather-container','')
+function toggleModale(el, content,style){
     el = document.querySelector(el)
     content = document.querySelector(content);
     el.addEventListener('click', (e)=>{
         content.style.display=style;
     })
 }
+const apiKey = '6bb83d7cf2b4aee765f7bd3f53260c22';
+
+form.addEventListener('click', (e)=>{
+    e.preventDefault();
+    let inp = input.value;
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${inp},${inp}&appid=${apiKey}`;
+    fetcj
+
+})
